fb6522492669dfd5bd12a18140a114080d08884a
Frank van der Heijden <frank.boekanier@gmail.com>
2020-12-27 15:50:26 +0100

    v1.1.0 - Conform to new Insights assistant update

--- src/main/java/net/frankheijden/insights/addons/bentobox/BentoBoxAssistant.java
+++ src/main/java/net/frankheijden/insights/addons/bentobox/BentoBoxAssistant.java
@@ -1,11 +1,14 @@
 package net.frankheijden.insights.addons.bentobox;
 
+import net.frankheijden.insights.entities.Area;
 import net.frankheijden.insights.entities.CacheAssistant;
-import net.frankheijden.insights.entities.Selection;
+import net.frankheijden.insights.entities.CuboidSelection;
 import org.bukkit.Location;
 import world.bentobox.bentobox.BentoBox;
 import world.bentobox.bentobox.database.objects.Island;
 import world.bentobox.bentobox.managers.IslandsManager;
+
+import java.util.Collections;
 
 public class BentoBoxAssistant extends CacheAssistant {
 
@@ -15,15 +18,19 @@ public class BentoBoxAssistant extends CacheAssistant {
      * There needs to be a constructor without parameters.
      */
     public BentoBoxAssistant() {
-        super("BentoBox", "island");
+        super("BentoBox", "BentoBox", "island", "v1.1.0");
     }
 
-    public Selection adapt(Island is) {
-        return new Selection(is.getWorld(), is.getProtectionBoundingBox());
+    public String getId(Island is) {
+        return getPluginName() + "@" + is.getUniqueId();
+    }
+
+    public Area adapt(Island is) {
+        return Area.from(this, getId(is), Collections.singletonList(new CuboidSelection(is.getWorld(), is.getProtectionBoundingBox())));
     }
 
     @Override
-    public Selection getSelection(Location location) {
+    public Area getArea(Location location) {
         return manager.getIslandAt(location)
                 .map(this::adapt)
                 .orElse(null);
